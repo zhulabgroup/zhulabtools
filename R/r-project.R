1bc9210ebf474da0e601ce86ed1bd7607c28781c
Kai Zhu <zhukai@umich.edu>
2025-05-13 11:43:12 -0400

    Refactor initialize_project into two separate functions--one for creating project subfolders and another for creating the .gitignore file

--- R/r-project.R
+++ R/r-project.R
@@ -1,35 +1,23 @@
-#' Initialize an R Project Structure
+#' Create Project Subfolders
 #'
-#' This function sets up a standardized file structure for an R project within a specified directory.
-#' It creates specified subfolders and a .gitignore file to manage unwanted files in version control.
+#' This function sets up specified subfolders within a project's root directory.
 #'
 #' @param project_path A character string specifying the path to the project root directory.
 #'   The default is the current working directory (".").
 #' @param subfolders A character vector indicating the names of subfolders to be created within
-#'   the project directory. The default subfolders are \code{c("R", "tests", "vignettes")}.
-#' @param overwrite_gitignore A logical value indicating whether to overwrite an existing
-#'   .gitignore file in the project directory. Defaults to \code{FALSE}.
+#'   the project directory. The default subfolders are \code{c("R", "vignettes")}.
 #'
-#' @return A logical value (\code{TRUE}) indicating if the initialization was successful.
-#'   The function is primarily used for its side-effect of creating folders and a .gitignore file.
+#' @return A logical value (\code{TRUE}) indicating if the subfolder creation was successful.
 #' @examples
 #' \dontrun{
-#' # Initialize project folders and .gitignore in the current directory
-#' initialize_project()
+#' # Create default subfolders in the current directory
+#' create_subfolders()
 #'
-#' # Initialize project folders and .gitignore in a specified directory
-#' initialize_project("/path/to/your/project")
-#'
-#' # Initialize project folders with custom subfolders and overwrite existing .gitignore file
-#' initialize_project(
-#'   project_path = "/path/to/your/project",
-#'   subfolders = c("R", "data", "docs"),
-#'   overwrite_gitignore = TRUE
-#' )
+#' # Create specified subfolders in a specified directory
+#' create_subfolders("/path/to/your/project", c("R", "vignettes"))
 #' }
-#' @seealso \code{\link{dir.create}}, \code{\link{file.path}}, \code{\link{writeLines}}
 #' @export
-initialize_project <- function(project_path = ".", subfolders = c("R", "tests", "vignettes"), overwrite_gitignore = FALSE) {
+create_subfolders <- function(project_path = ".", subfolders = c("R", "vignettes")) {
   # Create the subfolders if they do not exist
   for (folder in subfolders) {
     folder_path <- file.path(project_path, folder)
@@ -40,7 +28,29 @@ initialize_project <- function(project_path = ".", subfolders = c("R", "tests", 
       message("Folder already exists: ", folder_path)
     }
   }
+  return(TRUE)
+}
 
+#' Create .gitignore File
+#'
+#' This function creates a .gitignore file in the project's root directory based on a template.
+#'
+#' @param project_path A character string specifying the path to the project root directory.
+#'   The default is the current working directory (".").
+#' @param overwrite_gitignore A logical value indicating whether to overwrite an existing
+#'   .gitignore file in the project directory. Defaults to \code{FALSE}.
+#'
+#' @return A logical value (\code{TRUE}) indicating if the .gitignore file creation was successful.
+#' @examples
+#' \dontrun{
+#' # Create or overwrite .gitignore in the current directory
+#' create_gitignore(overwrite_gitignore = TRUE)
+#'
+#' # Create .gitignore in a specified directory without overwriting
+#' create_gitignore("/path/to/your/project")
+#' }
+#' @export
+create_gitignore <- function(project_path = ".", overwrite_gitignore = FALSE) {
   # Define path to the template .gitignore file
   template_gitignore_path <- system.file("templates/gitignore_template.txt", package = "zhulabtools")
 
